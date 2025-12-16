f0509b62b180fcc204450f78d22afcc6e9ff8407
Aadhi Senthil Kumar <aadhi.senthilkumar06@gmail.com>
2025-12-16 18:44:02 +1100

    changed up hero section for website, and added a feature section to elaborate on key features of our product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import HeroSection from "./landing/components/hero-section";
+import Features from "./landing/components/features-3";
 
 export default function Home() {
-  return <HeroSection />; 
+  return (
+    <div>
+      <HeroSection />
+      <Features />
+    </div>
+  ); 
 }
